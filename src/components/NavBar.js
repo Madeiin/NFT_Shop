c82939011c9918c68c38fec14e610fe1189eb464
ANONYMOUS-FRI3ND <anonymous_fri3nd@mail.ru>
2022-04-14 12:36:39 +0400

    Очко4

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -45,7 +45,7 @@ const NavBar = observer(() => {
                         <Button
                             variant={"outline-light"}
                             onClick={() => navigate(AUTH_ROUTE)}
-                        >ОЧко3
+                        >ОЧко4
                         </Button>
 
                     </Nav>
