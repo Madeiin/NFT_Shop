e5d51a60bc4721832966e5d48df715daa01e64c6
Madeiin <67866151+Madeiin@users.noreply.github.com>
2022-04-14 12:10:54 +0400

    login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -45,7 +45,7 @@ const NavBar = observer(() => {
                         <Button
                             variant={"outline-light"}
                             onClick={() => navigate(AUTH_ROUTE)}
-                        >ЗДАРОВА
+                        >Login
                         </Button>
 
                     </Nav>
